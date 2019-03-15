588b557a4267bffbd4274533e04a416a4a6d77a9
KapilDhall <dhall.kapil@gmail.com>
2019-03-15 12:03:48 +0530

    Adding code

---
+++ TestExerciseCode/TestExerciseCode/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {        
+
+        public static void Main(string[] args)
+        {                       
+            try
+            {
+                if (args.Length == 2)
+                {
+                    string identifier = args[0];
+                    string filepath = args[1];
+
+                    string output = "";
+                    if (identifier != "" && filepath != "")
+                    {
+                        output = GetOutput(identifier, filepath);
+                        Console.WriteLine("Output is " + output);
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Two arguments are mandatory");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect number of arguments. Please pass two arguments");
+                    Console.ReadLine();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            
+
+        }
+
+        
+
+        public static string GetOutput(string identifier, string filepath)
+        {
+            string output = String.Empty;
+            if (identifier == "" || filepath == "")
+            {
+                throw new Exception("identifier and filepath are mandatory");
+                //return output;
+            }
+
+            
+            List<string> list1 = new List<string>();
+            List<string> list2 = new List<string>();
+
+            try
+            {
+                list1.Add("-v");
+                list1.Add("--v");
+                list1.Add("/v");
+                list1.Add("--version");
+
+                list2.Add("-s");
+                list2.Add("--s");
+                list2.Add("/s");
+                list2.Add("--size");
+
+                FileDetails filedetails = new FileDetails();
+
+                if (list1.Contains(identifier))
+                {                    
+                    output = filedetails.Version(filepath);
+                }
+
+                else if (list2.Contains(identifier))
+                {                    
+                    output = Convert.ToString(filedetails.Size(filepath));
+                }
+
+                else
+                {
+                    output = "Incorrect identifier; Please check";
+                }
+            }
+            catch (Exception ex)
+            {
+                output = "Exception occurred: " + ex.Message + ".Please pass proper identifier";
+            }
+
+            list1 = null;
+            list2 = null;
+            return output;
+        }
+                
+    }
+}
---
+++ TestExerciseCode/TestProject1/ProgramTest.cs
@@ -0,0 +1,167 @@
+﻿using FileData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace TestProject1
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ProgramTest and is intended
+    ///to contain all ProgramTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ProgramTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for GetVersion
+        ///</summary>
+        [TestMethod()]
+        public void GetOutputVersionPositiveTest1()
+        {
+            string identifier = "-v"; // TODO: Initialize to an appropriate value
+            string filepath = "C:\\test.txt"; // TODO: Initialize to an appropriate value
+            //string expected = "We should have two dots(.) in Version"; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = Program.GetOutput(identifier, filepath);
+            var count = actual.Count(x => x == '.');
+                                               
+            Assert.AreEqual(2, count);
+        }
+
+        [TestMethod()]
+        public void GetOutputVersionNegativeTest1()
+        {
+            string identifier = "-ver"; // TODO: Initialize to an appropriate value
+            string filepath = "C:\\test.txt"; // TODO: Initialize to an appropriate value
+            string expected = "Incorrect identifier; Please check"; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = Program.GetOutput(identifier, filepath);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+       
+        [TestMethod()]
+        public void GetOutputExceptionTest()
+        {
+            string identifier = ""; // TODO: Initialize to an appropriate value
+            string filepath = "C:\\test.txt"; // TODO: Initialize to an appropriate value
+            string expected = "identifier and filepath are mandatory"; // TODO: Initialize to an appropriate value
+            string actual;
+            try
+            {
+                
+                actual = Program.GetOutput(identifier, filepath);
+
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("identifier and filepath are mandatory"))
+                {
+                    actual = "identifier and filepath are mandatory";
+                    Assert.AreEqual(expected, actual);
+                }
+            }                    
+
+            
+        }
+
+        [TestMethod()]
+        public void GetOutputSizePositiveTest1()
+        {
+            string identifier = "--size"; // TODO: Initialize to an appropriate value
+            string filepath = "C:\\test.txt"; // TODO: Initialize to an appropriate value
+            //string expected = "We should have single number without any space"; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = Program.GetOutput(identifier, filepath);
+            var count = actual.Count(x => x == ' ');
+
+            Assert.AreEqual(0, count);
+        }
+
+        [TestMethod()]
+        public void GetOutputSizePositiveTest2()
+        {
+            string identifier = "--size"; // TODO: Initialize to an appropriate value
+            string filepath = "C:\\test.txt"; // TODO: Initialize to an appropriate value
+            //string expected = "We should have single number without any space"; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = Program.GetOutput(identifier, filepath);
+
+            int result;
+            bool success = int.TryParse(actual, out result);
+
+            if (!success)
+            {
+                Assert.Fail("Cannot convert into int. Test Case failed");
+            }
+
+            
+        }
+
+        [TestMethod()]
+        public void GetOutputSizeNegativeTest1()
+        {
+            string identifier = "-sa"; // TODO: Initialize to an appropriate value
+            string filepath = "C:\\test.txt"; // TODO: Initialize to an appropriate value
+            string expected = "Incorrect identifier; Please check"; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = Program.GetOutput(identifier, filepath);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
